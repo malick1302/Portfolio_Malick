176fe14168cbc6c5fed9d19d10846cb1e7619a38
Brice Bammez <bricebammez@MacBook-Pro-de-Brice.local>
2025-04-10 17:22:31 +0200

    Add all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './index.css'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Competences from './pages/Competences'
