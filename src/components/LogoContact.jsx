335c376f76655607cfc54ed1307e4e04c3d8bc41
Brice Bammez <bricebammez@MacBook-Pro-de-Brice.local>
2025-04-14 01:07:52 +0200

    Add responsive and new texts

--- src/components/LogoContact.jsx
+++ src/components/LogoContact.jsx
@@ -8,10 +8,10 @@ function LogoContact({img}) {
 
     <div className='img w-200 h-12 m-12 flex justify-end md:justify-start md:m-0  md:w-100 md:ml-25 md:mb-13'>
         <a  href="https://www.linkedin.com/in/malick-baguiri/" target="_blank" rel="noopener noreferrer">
-        <img src={Linkedin} alt="logo Linkedin" className='w-20 '></img>
+        <img src={Linkedin} alt="logo Linkedin" className='max-md:w-10 w-20 '></img>
         </a>
         <a href="https://github.com/malick1302" target="_blank" rel="noopener noreferrer">
-        <img src={Git} alt="logo GitHub" className='w-20'></img>
+        <img src={Git} alt="logo GitHub" className=' max-md:w-10 w-20'></img>
         </a>
     </div>
    )
